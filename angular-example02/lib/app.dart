1872aa18e68faf1003f05ca2fbd0694c69b9b89e
Takayuki Hayashi <tkbyth@gmail.com>
2016-07-28 00:54:58 +0900

    example02

--- angular-example02/lib/app.dart
+++ angular-example02/lib/app.dart
@@ -4,4 +4,28 @@
 import 'package:angular2/core.dart';
 
 @Component(selector: 'app', templateUrl: 'app.html')
-class App {}
+class App {
+  String hoge1 = "hoge1";
+  String hoge2 = null;
+  bool hoge3 = true;
+
+  bool isDisplay(String str) {
+    return "str" == str;
+  }
+
+  bool isDisplayByBool(bool display) {
+    return display;
+  }
+
+  String getString() {
+    return "str";
+  }
+
+  bool isProduction(){
+    return false;
+  }
+
+  bool isSample(){
+    return true;
+  }
+}
